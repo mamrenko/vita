1444130527735f76cd74387e33f64c936921bb3a
mamrenko <Alex@host-192-168-0-106.starnet.ru>
2013-10-14 18:10:27 +0400

    Add Controller page

---
+++ application/classes/controller/index/page.php
@@ -0,0 +1,32 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * Страницы
+ */
+class Controller_Index_Page extends Controller_Index {
+
+    public function action_index() {
+        $this->action_static();
+    }
+
+    // Статические страницы
+    public function action_static() {
+        
+        $content = View::factory('index/page/v_page_static');
+        
+        // Выводим в шаблон
+        $this->template->page_title = 'Страница';
+        $this->template->block_center = array($content);
+    }
+
+    // Контакты
+    public function action_contacts() {
+
+        $content = View::factory('index/page/v_page_contacts');
+
+        // Выводим в шаблон
+        $this->template->page_title = 'Контакты';
+        $this->template->block_center = array($content);
+    }
+
+
+}
