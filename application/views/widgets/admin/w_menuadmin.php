1acda9603ae5289e3d8408ba7764c4973c23057d
mamrenko <Alex@iMac-Alexandra.local>
2013-10-15 19:00:56 +0400

    Add <?=URL::base()?>

--- application/views/widgets/admin/w_menuadmin.php
+++ application/views/widgets/admin/w_menuadmin.php
@@ -1,6 +1,6 @@
-<a href="/admin">Главная</a>
+<a href="<?=URL::base()?>admin">Главная</a>
 <a href="<?=URL::base()?>admin/orders">Заказы</a>
-<a href="/admin/pages">Страницы</a>
-<a href="/admin/products">Товары</a>
-<a href="/admin/users">Пользователи</a>
-<a href="/admin/settings">Настройки</a>
+<a href="<?=URL::base()?>admin/pages">Страницы</a>
+<a href="<?=URL::base()?>admin/products">Товары</a>
+<a href="<?=URL::base()?>admin/users">Пользователи</a>
+<a href="<?=URL::base()?>admin/settings">Настройки</a>
